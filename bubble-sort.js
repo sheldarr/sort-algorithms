5806ab7e7c8b0852e19dbe8bb52e783a65d6460f
sheldar@wp.pl <sheldar@wp.pl>
2016-05-24 18:49:06 +0200

    Bubble sort implementation

---
+++ bubble-sort.js
@@ -0,0 +1,25 @@
+function bubbleSort(array) {
+    var swapped = false;
+    var iterations = 0;
+
+    do {
+        iterations += 1;
+        swapped = false;
+
+        console.log(array);
+
+        for(var i=0; i<array.length; i++) {
+            if(array[i+1] < array[i]) {
+                var temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
+
+                swapped = true;
+            }
+        }
+    } while(swapped);
+
+    return iterations;
+}
+
+module.exports = bubbleSort;
